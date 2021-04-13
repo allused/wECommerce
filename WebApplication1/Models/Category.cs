e813dbeb611fb6ad0e513612197eb49e31806459
kalacs <divixed12@gmail.com>
2021-04-13 11:48:23 +0200

    Add base Product, Category, Picture, AppDbContext models

---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Category name required")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Picture
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Picture ID is required")]
+        public string ImageId { get; set; }
+        [Required(ErrorMessage = "You have to choose if it's a thumbnail")]
+        public bool IsThumbNail { get; set; }
+    }
+}
