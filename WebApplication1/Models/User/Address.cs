3b2d7cb31c00e9472eb4df25e04624c6b9484c7e
kalacs <divixed12@gmail.com>
2021-04-13 13:34:12 +0200

    Add Customer, Address base classes, move to User folder

---
+++ WebApplication1/Models/User/Address.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wECommerce.Models
+{
+    public class Address
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Customer ID is required")]
+        public Customer Customer { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string CustomerAddress { get; set; }
+
+        [Required(ErrorMessage = "Zip Code is required")]
+        public int ZipCode { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "State is required")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "Is Billing Address is required")]
+        public bool IsBillingAddress { get; set; }
+    }
+}
---
+++ WebApplication1/Models/User/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wECommerce.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; }
+        public List<Address> Addresses { get; set; }
+    }
+}
