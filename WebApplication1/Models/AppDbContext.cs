3ca76290cefa00fc54a6d6448a60c61ff40fd1f6
kalacs <divixed12@gmail.com>
2021-04-13 14:55:30 +0200

    Change DbContext to IdentityDbContext, move to Models folder

---
+++ WebApplication1/Models/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wECommerce.Models.Store;
+
+namespace WebApplication1.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>()
+                .Property(b => b.InStock)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<Product>()
+                .Property(b => b.IsOnSale)
+                .HasDefaultValue(false);
+
+            modelBuilder.Entity<Category>()
+                .Property(b => b.Description)
+                .HasDefaultValue("Description was not provided for this category.");
+
+            modelBuilder.Entity<CartItem>()
+                .Property(b => b.Quantity)
+                .HasDefaultValue();
+
+            modelBuilder.Entity<CartItem>()
+                .Property(b => b.CreatedAt)
+                .HasDefaultValueSql("getdate()");
+        }
+    }
+}
