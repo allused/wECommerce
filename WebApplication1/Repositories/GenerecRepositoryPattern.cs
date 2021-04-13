4b18559203cb791c30dbdf1fcd9c1bfa587a2c69
kalacs <divixed12@gmail.com>
2021-04-13 13:11:25 +0200

    Add GenereicRerpository interface

---
+++ WebApplication1/Repositories/GenerecRepositoryPattern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wECommerce.Repositories
+{
+    public interface IGenerecRepositoryPattern<T> where T : class
+    {
+        Task<T> FindAsync(object id);
+        Task AddAsync(T obj);
+        void Update(T obj);
+        void Remove(T obj);
+    }
+}
