f25b4385413e6aaf4c6a1a3e6da0679a84c83007
kalacs <divixed12@gmail.com>
2021-04-13 14:14:57 +0200

    Add CartItem base class

---
+++ WebApplication1/Models/Store/CartItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace wECommerce.Models.Store
+{
+    public class CartItem
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Product ID is required")]
+        public Product Product { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+    }
+}
