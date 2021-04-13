1f4e330adbb9703e87c2fbf003b48b5e75ad4047
kalacs <divixed12@gmail.com>
2021-04-13 14:14:31 +0200

    Add Cart base class

---
+++ WebApplication1/Models/Store/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wECommerce.Models.Store
+{
+    public class Cart
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Customer ID is required")]
+        public Customer Customer { get; set; }
+        public List<CartItem> CartItems { get; set; }
+
+    }
+}
