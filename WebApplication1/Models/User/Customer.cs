96d446fa436dfe49407d952d20a4cab748b29df1
kalacs <divixed12@gmail.com>
2021-04-23 13:11:22 +0200

    Modify Customer model, inherit from Identity User

--- WebApplication1/Models/User/Customer.cs
+++ WebApplication1/Models/User/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace wECommerce.Models
 {
-    public class Customer
+    public class Customer : IdentityUser
     {
-        public long Id { get; set; }
+        /*public long Id { get; set; }*/
 
         [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
@@ -16,11 +17,11 @@ namespace wECommerce.Models
         [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Email address is required")]
-        public string EmailAddress { get; set; }
+        /*[Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }*/
 
-        [Required(ErrorMessage = "Phone number is required")]
-        public string PhoneNumber { get; set; }
+        /*[Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; }*/
         public List<Address> Addresses { get; set; }
     }
 }
