d557416250059509da29d3c9eea0c5458b6faa42
kalacs <divixed12@gmail.com>
2021-04-23 12:40:12 +0200

    Add User service response models

---
+++ WebApplication1/Models/User/UserManagerResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wECommerce.Models.User
+{
+    [NotMapped]
+    public class UserManagerResponse
+    {
+
+        public string Message { get; set; }
+        public bool IsSuccesful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
