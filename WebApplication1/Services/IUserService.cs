7d80f6359ba93a2ddf4b73539d755f402ab17e4d
kalacs <divixed12@gmail.com>
2021-04-23 12:40:51 +0200

    Add User Register service

---
+++ WebApplication1/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wECommerce.Models.User;
+
+namespace wECommerce.Services
+{
+    public interface IUserService
+    {
+        Task<UserManagerResponse> RegisterUserAsync(RegisterUser user);
+
+    }
+}
---
+++ WebApplication1/Services/UserService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wECommerce.Models.User;
+
+namespace wECommerce.Services
+{
+    public class UserService : IUserService
+    {
+        private UserManager<IdentityUser> userManager;
+
+        public UserService(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterUser user)
+        {
+
+            if (user == null)
+            {
+                throw new NullReferenceException("Register User Model is null");
+            }
+            if (user.Password != user.ConfirmPassword)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "Confirmation password doesn't match the password",
+                    IsSuccesful = false
+                };
+            }
+            var identityUser = new IdentityUser
+            {
+                Email = user.Email,
+                UserName = user.Email
+            };
+
+            var result = await userManager.CreateAsync(identityUser, user.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "Register succesful",
+                    IsSuccesful = true
+                };
+            }
+
+            return new UserManagerResponse
+            {
+                Message = "User did not create",
+                IsSuccesful = false,
+                Errors = result.Errors.Select(e => e.Description)
+            };
+        }
+    }
+}
