d3b75b42c1e04733668b7d1b53342c7005a1f53a
kalacs <divixed12@gmail.com>
2021-04-23 12:38:56 +0200

    Add user services

---
+++ WebApplication1/Controllers/User/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wECommerce.Models.User;
+using wECommerce.Services;
+
+namespace wECommerce.Controllers.User
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IUserService userService;
+
+        public AuthController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterUser userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await userService.RegisterUserAsync(userModel);
+
+                if (result.IsSuccesful)
+                {
+                    return Ok(result);
+                }
+                return BadRequest(result);
+            }
+
+            return BadRequest("Some properties are invalid");
+        }
+    }
+}
